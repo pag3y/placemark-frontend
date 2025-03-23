a30c2cc56da9991a0dbb2137ae46efb5a1ef3c72
pag3y <page0391@gmail.com>
2025-03-23 19:36:34 +0000

    Deployment commits

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://placemark-backend-y2j0.onrender.com',
 });
 
 export default api;
